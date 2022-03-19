3baaf80b50c45cf502f4b3387f41bc34ceebcb03
Kushal Aswani <kushal.aswani@s.amity.edu>
2022-03-19 17:36:55 +0530

    Added the Producer View

--- Models/Producer.cs
+++ Models/Producer.cs
@@ -10,10 +10,14 @@ namespace eTickets.Models
     {
         [Key]
         public int ID { get; set; }
+
+        [Display(Name = "Profile Picture")]
         public string ProfilePictureURL { get; set; }
 
+        [Display(Name = "Full Name")]
         public string FullName { get; set; }
 
+        [Display(Name = "Bio")]
         public string Bio { get; set; }
 
         //Relationships
