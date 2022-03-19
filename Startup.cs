7c33c87bed983ee78e5f6d640a92fc8213ac281f
Kushal Aswani <kushal.aswani@s.amity.edu>
2022-03-19 15:58:56 +0530

    Seeding the Database

--- Startup.cs
+++ Startup.cs
@@ -57,6 +57,9 @@ namespace eTickets
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            //Seed the Database
+            AppDbInitializer.Seed(app);
         }
     }
 }
